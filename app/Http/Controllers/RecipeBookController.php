d0b1ffd9c44d6c741932138c55beade3b6765987
Mark Neil <mark.neil.u.cordero@gmail.com>
2024-03-16 16:42:27 +0800

    adding laravel breeze and adding model, controller and requests

---
+++ app/Http/Controllers/RecipeBookController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RecipeBook;
+use App\Http\Requests\StoreRecipeBookRequest;
+use App\Http\Requests\UpdateRecipeBookRequest;
+
+class RecipeBookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRecipeBookRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(RecipeBook $recipeBook)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(RecipeBook $recipeBook)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRecipeBookRequest $request, RecipeBook $recipeBook)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(RecipeBook $recipeBook)
+    {
+        //
+    }
+}
