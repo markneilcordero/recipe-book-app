14968e7a066987b76f31156c0a78d2210ca57e7c
Mark Neil <mark.neil.u.cordero@gmail.com>
2024-04-29 13:34:45 +0800

    adding Model, Migration, Request, and Controller

---
+++ app/Http/Controllers/RecipebookController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Recipebook;
+use App\Http\Requests\StoreRecipebookRequest;
+use App\Http\Requests\UpdateRecipebookRequest;
+
+class RecipebookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRecipebookRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Recipebook $recipebook)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Recipebook $recipebook)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRecipebookRequest $request, Recipebook $recipebook)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Recipebook $recipebook)
+    {
+        //
+    }
+}
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!Doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Bootstrap demo</title>
+@vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+
+<div class="alert alert-danger" role="alert">
+  A simple danger alert—check it out!
+</div>
+
+</body>
+</html>
