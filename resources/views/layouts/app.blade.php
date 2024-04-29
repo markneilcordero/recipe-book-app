efb5fc92016c0ad14b8ec58bbdbde0c183d3d814
Mark Neil <mark.neil.u.cordero@gmail.com>
2024-04-29 15:52:55 +0800

    testing bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Bootstrap demo</title>
-@vite(['resources/css/app.css', 'resources/js/app.js'])
+@vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
 
